7d6fc30fad4646e7c72fd89f77b0817ca5428054
Martin Wambugu <32651140+martinwamb@users.noreply.github.com>
2026-06-05 17:33:24 +0300

    Integrate Android touch controls and back button functionality
    Auto-merged after code review.

---
+++ src/input/AndroidTouchInput.gd
@@ -0,0 +1,29 @@
+# 
+extends Node
+
+func _ready():
+    input_map["ui_left"] = InputEventScreenEdgeSwiped.LEFT
+    input_map["ui_right"] = InputEventScreenEdgeSwiped.RIGHT
+    input_map["ui_up"] = InputEventScreenEdgeSwiped.TOP
+    input_map["ui_down"] = InputEventScreenEdgeSwiped.BOTTOM
+
+func _input(event):
+    if event is InputEventScreenEdgeSwiped:
+        match event.get_edge():
+            SCREEN_EDGE_LEFT: # Left swipe to go left
+                if get_node("Car"): # Assuming the car movement node exists
+                    get_node("Car").go_left()
+            SCREEN_EDGE_RIGHT: # Right swipe to go right
+                if get_node("Car"): 
+                    get_node("Car").go_right()
+
+# 
+extends Node
+
+func _ready():
+    Input.map_action("back", "ui_back")
+
+func _unhandled_input(event):
+    if event is InputEventAction and event.action == "ui_back":
+        if Application.is_running():
+            quit()
